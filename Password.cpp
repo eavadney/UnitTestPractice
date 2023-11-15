25f7db5b983c74aec7f519ae4489757f76445413
eavadney <eavadney@mail.csuchico.edu>
2023-11-15 11:52:03 -0800

    11/15 class

--- Password.cpp
+++ Password.cpp
@@ -1,5 +1,6 @@
 #include "Password.h"
 #include <string>
+#include <cctype>
 
 using std::string;
 
@@ -24,15 +25,18 @@ int Password::count_leading_characters(string phrase){
   returns whether the phrase has both at least one upper-case letter and
   at least one lower-case letter
 */
-bool has_mixed_case(string phrase){
+
+//mine. Not passing some basic tests.
+
+bool Password::has_mixed_case(string phrase){
   bool hasUpper = false;
   bool hasLower = false;
 
   for (int i = 0; i < phrase.length(); i++){
-	  if (isupper(phrase[i]) == true){
+	  if (isupper(phrase[i])){
 		  hasUpper = true;
 	  }
-	  if (islower(phrase[i]) == true){
+	  if (islower(phrase[i])){
 		  hasLower = true;
 	  }
 	  if ((hasUpper == true) && (hasLower == true)){
@@ -43,3 +47,60 @@ bool has_mixed_case(string phrase){
   return false;
 }
 
+
+//outside version from git
+/*
+bool Password::has_mixed_case(string pass){
+  bool lower = false;
+  bool upper = false;
+  for(int i = 0; i < pass.size(); i++){
+    if( pass[i] >= 'A' && pass[i] <= 'Z' ){
+      upper = true;
+    }
+    else if( pass[i] < 'z'){
+      lower = true;
+    }
+  }
+  return upper && lower;
+}
+*/
+
+
+/*
+  receives a password and sets the latest in pass_history to it if and only
+  if it meets all criteria:
+    1. The password is at least 8 letters long
+    2. It has no more than 3 of the same leading characters
+    3. It has mixed case (at least one upper case and at least one lower case)
+    4. It was not a previous password in the history
+*/
+void Password::set(string pass){
+  if(pass.size() >= 8){
+	  if(count_leading_characters(pass) <= 3){
+		  if(has_mixed_case(pass)){
+			  for(int i = 0; i < pass_history.size(); i++){
+				  if(pass == pass_history[i]){
+					  return; //password in history. return without adding it
+				  }
+			  }
+			  //password was not in history
+			  pass_history.push_back(pass);
+		  }
+	  }
+  }
+}
+
+/*
+  receives a string and authenticates it against the latest password in the
+  pass_history, returning true for an exact match or false when it does not match
+  or if a password has not been set.
+*/
+bool Password::authenticate(string pass){
+	if(pass_history.size() == 0){
+		return false;
+	}
+	if(pass == pass_history.back()){
+		return true;
+	}
+	return false;
+}
--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -27,7 +27,7 @@ TEST(PasswordTest, caps_diffrent)
 	ASSERT_EQ(2,actual);
 
 }
-TEST(PasswordTest, empty_string)
+TEST(PasswordTest, empty_string_leading)
 {
 	Password pass;
 	int actual = pass.count_leading_characters("");
@@ -39,10 +39,91 @@ TEST(PasswordTest, later_rep)
 	int actual = pass.count_leading_characters("abbbbbc");
 	ASSERT_EQ(1,actual);
 }
-TEST(PasswordTest, lower_and_cap)
+TEST(PasswordTest, lower_then_cap)
 {
 	Password pass;
-	bool both = pass.has_mixed_case("zZ");
+	bool both = pass.has_mixed_case("zzzzzZ");
 	ASSERT_EQ(true, both);
+}
+TEST(PasswordTest, empty_string_caps)
+{
+	Password pass;
+	bool both = pass.has_mixed_case("");
+	ASSERT_EQ(false, both);
+}
+TEST(PasswordTest, cap_then_lower)
+{
+	Password pass;
+	bool both = pass.has_mixed_case("ZZZZZZz");
+	ASSERT_EQ(true, both);
+}
+TEST(PasswordTest, only_lower)
+{
+	Password pass;
+	bool both = pass.has_mixed_case("zzz");
+	ASSERT_EQ(false, both);
+}
+TEST(PasswordTest, only_upper)
+{
+	Password pass;
+	bool both = pass.has_mixed_case("ZZZ");
+	ASSERT_EQ(false, both);
+}
+TEST(PasswordTest, caps_if_non_aphanum)
+{
+	Password pass;
+	bool both = pass.has_mixed_case("!@#$%^&a");
+	ASSERT_EQ(false, both);
+}
+TEST(PasswordTest, set_too_short)
+{
+	Password pass;
+	pass.set("12345aA");
+	bool authed = pass.authenticate("12345aA");
+	ASSERT_EQ(authed, false);
+}
+TEST(PasswordTest, set_too_many_leading)
+{
+	Password pass;
+	pass.set("aaaaBbbbbbb");
+	bool authed = pass.authenticate("aaaaBbbbbbb");
+	ASSERT_EQ(authed, false);
+}
+TEST(PasswordTest, set_not_mixed_case)
+{
+	Password pass;
+	pass.set("aaabbbbbbb");
+	bool authed = pass.authenticate("aaabbbbbbb");
+	ASSERT_EQ(authed, false);
+}
+TEST(PasswordTest, set_already_password)
+{
+	Password pass;
+	pass.set("myPassword");
+	pass.set("myPassword");
+	bool authed = pass.authenticate("myPassword");
+	ASSERT_EQ(authed, true);
+}
+TEST(PasswordTest, auth_newest_pass)
+{
+	Password pass;
+	pass.set("myPassword");
+	pass.set("myNewPassword");
+	bool authed = pass.authenticate("myNewPassword");
+	ASSERT_EQ(authed, true);
+}
+TEST(PasswordTest, auth_old_pass)
+{
+	Password pass;
+	pass.set("myPassword");
+	pass.set("myNewPassword");
+	bool authed = pass.authenticate("myPassword");
+	ASSERT_EQ(authed, false);
+}
+TEST(PasswordTest, auth_none_set)
+{
+	Password pass;
+	bool authed = pass.authenticate("myPasword");
+	ASSERT_EQ(authed, false);
 }
 
